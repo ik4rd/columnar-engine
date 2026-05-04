46a86895964f7cd67995605e14fed664d938015c
ik4rd <treep.me@yandex.ru>
2026-05-04 18:22:12 +0300

    feat: files removed for review

--- src/model/column_boolean.cpp
+++
@@ -1,7 +0,0 @@
-#include "column_boolean.h"
-
-#include "parsing.h"
-
-void BooleanColumn::AppendFromString(const std::string& value) { AppendValue(ParseBoolean(value) ? 1 : 0); }
-
-std::string BooleanColumn::ValueAsString(const size_t row) const { return BooleanToString(ValueAt(row) != 0); }
--- src/model/column_timestamp.cpp
+++
@@ -1,7 +0,0 @@
-#include "column_timestamp.h"
-
-#include "parsing.h"
-
-void TimestampColumn::AppendFromString(const std::string& value) { AppendValue(ParseTimestamp(value)); }
-
-std::string TimestampColumn::ValueAsString(const size_t row) const { return TimestampToString(ValueAt(row)); }
